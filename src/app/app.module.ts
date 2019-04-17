8554f248dd714c2080e5ae14ec8bc26a25e9ddf5
Grimto <safouene.chahed@medtech.tn>
2019-04-18 01:00:04 +0200

    Project before server connection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ContactComponent } from './contact/contact.component';
+import { DemeterSlogComponent } from './demeter-slog/demeter-slog.component';
+import { DownloadComponent } from './download/download.component';
+import { FeaturesComponent } from './features/features.component';
+import { TeamComponent } from './team/team.component';
+import {RouterModule} from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ContactComponent,
+    DemeterSlogComponent,
+    DownloadComponent,
+    FeaturesComponent,
+    TeamComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
